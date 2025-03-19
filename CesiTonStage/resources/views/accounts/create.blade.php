276ff52704a5af4297e5289a8ec7c1bbeb013bf8
a <mathis.vogel@viacesi.fr>
2025-03-19 23:54:13 +0100

    patch/

---
+++ CesiTonStage/resources/views/accounts/create.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-100 mx-auto bg-white shadow-lg rounded-lg p-6 border mt-4">
+        <h2 class="text-xl font-semibold text-gray-800 mb-4">Créer un compte</h2>
+
+        @if (session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        @if ($errors->any())
+            <div class="text-red-500">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('accounts.store') }}" method="POST" class="flex flex-col justify-center">
+            @csrf
+
+            <div class="mb-4">
+                <label for="Email_Account" class="block text-gray-700">Email :</label>
+                <input value="{{ old('Email_Account') }}" type="email" name="Email_Account" id="Email_Account" class="w-full px-4 py-2 border rounded-lg"
+                    required>
+            </div>
+
+            <div class="mb-4">
+                <label for="Password_Account" class="block text-gray-700">Mot de passe :</label>
+                <input value="{{ old('Password_Account') }}" type="password" name="Password_Account" id="Password_Account"
+                    class="w-full px-4 py-2 border rounded-lg" required>
+            </div>
+
+            <div class="mb-4">
+                <label for="First_name_Account" class="block text-gray-700">Prénom :</label>
+                <input value="{{ old('First_name_Account') }}" type="text" name="First_name_Account" id="First_name_Account"
+                    class="w-full px-4 py-2 border rounded-lg" required>
+            </div>
+
+            <div class="mb-4">
+                <label for="Last_name_Account" class="block text-gray-700">Nom :</label>
+                <input value="{{ old('Last_name_Account') }}" type="text" name="Last_name_Account" id="Last_name_Account"
+                    class="w-full px-4 py-2 border rounded-lg" required>
+            </div>
+
+            <div class="mb-4">
+                <label for="Birth_date_Account" class="block text-gray-700">Date de naissance :</label>
+                <input value="{{ old('Birth_date_Account') }}" type="date" name="Birth_date_Account" id="Birth_date_Account"
+                    class="w-full px-4 py-2 border rounded-lg" required>
+            </div>
+
+            <div class="mb-4">
+                <label value="{{ old('Id_Role') }}" for="Id_Role" class="block text-gray-700">Rôle :</label>
+                <select name="Id_Role" id="Id_Role" class="w-full px-4 py-2 border rounded-lg" required>
+                    <option value="1">Étudiant</option>
+                    <option value="2">Pilote</option>
+                    <option value="3">Admin</option>
+                </select>
+            </div>
+
+            <div class="mb-4 flex items-center">
+                <label for="remember" class="text-zinc-700 mr-2">Se souvenir de moi</label>
+                <input type="checkbox" name="remember" id="remember" class="form-check-input">
+            </div>
+
+            <button type="submit"
+                class="bg-white text-yellow-500 px-4 py-2 rounded h-min border-yellow-500 border-2 hover:bg-yellow-300 hover:text-black">Créer
+                le compte</button>
+        </form>
+    </div>
+@endsection
--- CesiTonStage/resources/views/layouts/login_app.blade.php
+++ CesiTonStage/resources/views/layouts/login_app.blade.php
@@ -15,12 +15,12 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
-<body class="flex flex-col min-h-screen">
+<body class="flex flex-col h-screen">
     <!-- Header -->
-    <header class="flex flex-col items-center bg-white w-full border shadow-gray-500 shadow-md">
+    <header class="flex flex-col items-center bg-white w-full border shadow-gray-500 shadow-md mb-[10vh]">
         
         <div class="logo my-4">
-            <img src="{{ asset('images/logod_petit.svg') }}" alt="Logo CTS" class="w-[10em] mx-auto">
+            <img src="{{ asset('images/logod_petit.svg') }}" alt="Logo CTS" class="w-[11em] mx-auto">
         </div>
              
     </header>
